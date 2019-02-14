bde3ea702ffe1b39de54b3687436b185d768cb24
jhoysa <jhoysa@macsales.com>
2019-02-14 16:02:00 -0600

    File setup

---
+++ change-class/script.js
@@ -0,0 +1,10 @@
+let toggleList = document.querySelector('.toggle-list-view');
+let toggleGrid = document.querySelector('.toggle-grid-view');
+
+toggleList.onclick = function() {
+  //Need to remove a class then add a class
+};
+
+toggleGrid.onclick = function() {
+  //Need to remove a class then add a class
+};
