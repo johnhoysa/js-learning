d5550a8a994eae7ee299f9ced8dca4851c736845
John Hoysa <johnhoysa@gmail.com>
2019-02-18 18:28:48 -0600

    Toggle working

--- change-class/script.js
+++ change-class/script.js
@@ -1,10 +1,25 @@
-let toggleList = document.querySelector('.toggle-list-view');
-let toggleGrid = document.querySelector('.toggle-grid-view');
+let toggleList = document.querySelector(".toggle-list-view");
+
+let toggleGrid = document.querySelector(".toggle-grid-view");
+
+let productTile = document.querySelectorAll(".change-view");
 
 toggleList.onclick = function() {
-  //Need to remove a class then add a class
+  for (let i = 0; i < productTile.length; i++) {
+    productTile[i].classList.add("list-view");
+    productTile[i].classList.remove("grid-view");
+  }
 };
 
 toggleGrid.onclick = function() {
-  //Need to remove a class then add a class
+  for (let i = 0; i < productTile.length; i++) {
+    productTile[i].classList.add("grid-view");
+    productTile[i].classList.remove("list-view");
+  }
 };
+
+// This works for one single item
+// toggleList.onclick = function() {
+//   productTile.classList.add("list-view");
+//   productTile.classList.remove("grid-view");
+// };
