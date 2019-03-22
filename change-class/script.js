2b1b5461fcdfc8d4ffa886d77fd0e2b0b3f12b03
jhoysa <jhoysa@macsales.com>
2019-03-22 10:23:18 -0500

    example for co-worker

--- change-class/script.js
+++ change-class/script.js
@@ -1,10 +1,15 @@
 let toggleList = document.querySelector('.toggle-list-view');
+
 let toggleGrid = document.querySelector('.toggle-grid-view');
 
+let tileWrap = document.querySelector('.content-wrap');
+
 toggleList.onclick = function() {
-  //Need to remove a class then add a class
+  tileWrap.classList.add('list-view');
+  tileWrap.classList.remove('grid-view');
 };
 
 toggleGrid.onclick = function() {
-  //Need to remove a class then add a class
+  tileWrap.classList.add('grid-view');
+  tileWrap.classList.remove('list-view');
 };
