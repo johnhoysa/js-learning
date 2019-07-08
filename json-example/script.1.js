da1251f9f102be300415b27fd6b7945f6fd07d3f
jhoysa <jhoysa@macsales.com>
2019-07-08 09:14:21 -0500

    updated some code

--- json-example/script.1.js
+++ json-example/script.1.js
@@ -1,5 +1,5 @@
 //Create variable for json data
-const requestURL = "https://ecn1.eshop2.macsales.com/api/used-macs/config/?item=UABF1D79OBX1XXC";
+const requestURL = "https://ecn1.eshop2.macsales.com/api/used-macs/config/?item=UAQA1JS6XXX10XI";
 
 const requestMac = new XMLHttpRequest();
 requestMac.open("GET", requestURL);
@@ -9,37 +9,57 @@ requestMac.send();
 requestMac.onload = function() {
   const usedMacRepsonse = requestMac.response;
   const systemInfo = JSON.parse(usedMacRepsonse);
-  usedMac(systemInfo);
+  heroUnit(systemInfo);
   highlights(systemInfo)
+
 };
 
-function usedMac(jsonObj) {
+function heroUnit(jsonObj) {
   const systemInfo = jsonObj;
+  //console.log(systemInfo);
   //
-  usedMacs.innerHTML +=`
+
+  usedMacs.innerHTML += `
   <div class="hero">
     <h1>${systemInfo.machineHeadline}</h1>
     <p>${systemInfo.systems[0].systemDetails}</p>
+    <p>${systemInfo.systems[0].options[1].optionGroupLabel}</p>
+    <p>${systemInfo.systems[0].options[1].optionValues[0].label}</p>
+    
   </div>
   `;
 }
+
 
 //Variables I need to use in the below loop
 //Best practice of location not sure of but I would think its at top of page 
 let highlightLabel = '';
 let highlightValue = '';
+let highlightList = '';
 //
 function highlights(jsonObj){
   const systemInfo = jsonObj;
   console.log(systemInfo.highlights.length);
   for (let i = 0; i < systemInfo.highlights.length; i++) {
     highlightLabel = '<p>' + systemInfo.highlights[i].label + '</p>';
-    highlightValue = '<li>' + systemInfo.highlights[i].values + '</li>';
+    highlightValue = '';
+    //
+    for (let j = 0; j < systemInfo.highlights[i].values.length; j++) {
+      console.log('line 47', systemInfo.highlights[i].values[j]);
+      //highlightValue += '<li>' + systemInfo.highlights[i].values[j] + '</li>';
+    }
     //loop through label and create list under it.
-    usedMacs.innerHTML +=`
+    highlightList +=`
     ${highlightLabel}
     <ul class="highlights-items">${highlightValue}</ul>
   `;
+   
   }
+  usedMacs.innerHTML +=`
+    <div class="container">
+      <div class="highlights">
+        ${highlightList}
+      </div>
+    </div>
+  `;
 }
-
