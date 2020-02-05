b193b4a42d735a6a8c6c55a5f273b21532c6d97e
jhoysa <jhoysa@macsales.com>
2020-02-05 09:30:50 -0600

    removed importJS for now

--- importjs/src/js/files/showData.js
+++
@@ -1,7 +0,0 @@
-import { getUsefulContents, data } from "../app.js";
-
-export function headings(data) {
-  for (let i = 0; i < data.group.length; i++) {
-    contentHere.innerHTML += `<h3>${data.group[i].headline}</h3>`;
-  }
-}
